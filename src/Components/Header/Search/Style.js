752a8d8beaaa9898140529c5ae0226e6036e91ce
Mariana Bálsamo <mariana.balsamo@intive.com>
2020-03-29 17:59:04 -0300

    TP5 search input done

---
+++ src/Components/Header/Search/Style.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/styles';
+
+export default makeStyles({
+  searchContainer: {
+    marginLeft: 50,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  lookingGlass: {
+    width: '18px',
+    marginRight: 10,
+  },
+  input: {
+    width: '800px',
+    height: '24px',
+    border: 'none',
+    fontSize: '16px',
+  },
+});
